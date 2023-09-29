e1147684627830adf150b2d9df70af2fb726bb0e
stasiupa <125651294+stasiupa@users.noreply.github.com>
2023-09-29 18:27:25 +0200

    fake-store-added
    fake store added in redeem subpage

--- app/redeem/page.tsx
+++ app/redeem/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import ProductItem from "../components/ProductItem";
+import ProductBoard from "../components/ProductBoard";
 
 function RedeemPage() {
   return (
     <div>
-      Products to get:
-      <div className="flex">
-        <ProductItem />
-        <ProductItem />
+      <div className="text-center p-2">You have /this amount/ of coins!</div>
+      <div className="text-center p-2">
+        Redeem Your coins in the Fake Store below!
+      </div>
+      <div>
+        Products to get:
+        <div className="flex">
+          <ProductBoard />
+        </div>
       </div>
     </div>
   );
